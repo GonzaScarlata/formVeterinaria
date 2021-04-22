84eda2741280ca5a3e79afa51247c0857457eb11
Gonzalo Scarlata <gonza.sc.gs@gmail.com>
2021-04-22 19:00:16 -0300

    agregué states, ahora a mejorarlos

--- src/components/ShiftsForm.jsx
+++ src/components/ShiftsForm.jsx
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
+import './shiftsForm.css';
 
 const ShiftsForm = () => {
+    const varMascota = 'mascota';
+    const varDuenio = 'duenio';
+    const varFecha = 'fecha';
+    const varHora = 'hora';
+    const varSintomas = 'sintomas';
+    const [shift, setShift] = useState({mascota:'', duenio:'', fecha: Date, hora: Date, sintomas:'', id:'' });
+    const [shiftsArray, setShiftsArray] = useState([]);
+
+    const generateId = function () {
+        return '_' + Math.random().toString(36).substr(2, 9);
+    };
+
+    const handleChange = (e, variable) => {
+        var temporalVar = e.target.value;
+        const id = generateId();
+
+        if (variable === 'mascota') {
+            setShift({...shift, mascota: temporalVar, id});
+        } else if (variable === 'duenio') {
+            setShift({...shift, duenio: temporalVar, id});
+        } else if (variable === 'fecha') {
+            setShift({...shift, fecha: temporalVar, id});
+        } else if (variable === 'sintomas') {
+            setShift({...shift, sintomas: temporalVar, id});
+        } else {
+            setShift({...shift, hora: temporalVar, id});
+        };
+    }
+
     return (
-        <section className='my-4'>
+        <section id='shifts' className='shadow-lg rounded container my-4'>
             <h1 className='text-center'>Solicitud de turno</h1>
             <Form className="w-75 container my-4">
                 <Form.Group className="d-inline-flex justify-content-around w-100 row">
@@ -14,6 +44,7 @@ const ShiftsForm = () => {
                         className="col-8"
                         type="text"
                         placeholder="Nombre de mascota"
+                        onChange={(e)=>handleChange(e, varMascota)}
                     />
                 </Form.Group>
 
@@ -26,6 +57,7 @@ const ShiftsForm = () => {
                         className="col-8"
                         type="text"
                         placeholder="Nombre del dueño"
+                        onChange={(e)=>handleChange(e, varDuenio)}
                     />
                 </Form.Group>
 
@@ -34,9 +66,9 @@ const ShiftsForm = () => {
                     controlId="formBasicPassword"
                 >
                     <Form.Label className="col-4">Fecha:</Form.Label>
-                    <Form.Control className="col-3" type="date" />
+                    <Form.Control className="col-3" type="date" onChange={(e)=>handleChange(e, varFecha)}/>
                     <Form.Label className="col">hora:</Form.Label>
-                    <Form.Control className="col-3" type="time" />
+                    <Form.Control className="col-3" type="time" onChange={(e)=>handleChange(e, varHora)}/>
                 </Form.Group>
 
                 <Form.Group
@@ -48,6 +80,7 @@ const ShiftsForm = () => {
                         className="col-8"
                         type="text-area"
                         placeholder="Describa los síntomas"
+                        onChange={(e)=>handleChange(e, varSintomas)}
                     />
                 </Form.Group>
                 <div className="text-center">
